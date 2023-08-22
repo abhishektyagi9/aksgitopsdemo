596bfd3ecec23d34d4a6397f92e2516d26e618c6
Abhishek Tyagi <atyagi@miceosoft.com>
2023-08-22 00:07:51 -0500

    trigger

--- bookinfo/src/ratings/ratings.js
+++ bookinfo/src/ratings/ratings.js
@@ -17,7 +17,7 @@ var dispatcher = require('httpdispatcher')
 
 var port = parseInt(process.argv[2])
 
-var userAddedRatings = [] // used to demonstrate POST functionality
+var userAddedRatings = [] // used to demonstrates POST functionality
 
 var unavailable = false
 var healthy = true
